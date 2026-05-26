b20476368ad9b22cb45e9a137195a47d839056a0
Diva <quocanhnnt@gmail.com>
2026-05-26 08:10:45 +0700

    Update greeting to student info

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello from Render!</title>
+    <title>Hello from 24520065-NguyenQuocAn</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
